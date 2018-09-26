7bb2c51f64fca87afe78f0cc6044aa82f5435ec0
TobyOne <24594009+MegaTobyOne@users.noreply.github.com>
2018-09-26 12:34:19 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
 <!-- Dummy file to initially try and workaround Heroku buildpacks -->
+
+<!-- index.html -->
+
+<HEAD>
+  <meta content="text/html; charset=utf-8" http-equiv="Content-type" />
+  <title>MILLIWAX | The Online Record Store </title>
+  <link href="./basic.css" rel="stylesheet" />
+  <link href="https://www.milliwax.com/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+</HEAD>
+<BODY>
+
+
+
+<div class="header-main">
+  <a class="header-logo" href="https://www.milliwax.com"></a>
+
+  <h1>Milliwax was always going to be an online Record Store</h1>
+  <h3>Others have done that so much better.</h3>
+
+</div>
+
+<p>Here are some records that I think you should consider buying.</p>
+
+<ul>
+  <li>Rainbow Rising - Rainbow</li>
+  <li>Sad Wings of Destiny - Judas Priest</li>
+  <li>Northern Exposure - Sasha and Digweed</li>
+</ul>
+<br>
+
+Footer goes here.<br>
+
+</BODY>
